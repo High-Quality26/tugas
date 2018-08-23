e0614996d5657336cf1a22c745ec6ea206db2a69
WINDOWS 10 <WINDOWS 10@169.254.2.144>
2018-08-23 11:54:10 +0700

    Modul2

---
+++ src/Modul2/Ujibola.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul2;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class Ujibola {
+    public static void main(String[] args){
+        bola ball = new bola ();
+        ball.setjarijari(14);
+        ball.showDiameter();
+        ball.showLuasPermukaan();
+        ball.showVolume();
+        ball.cetak();
+    }
+}
---
+++ src/Modul2/bola.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul2;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class bola {
+    public double jarijari=14;
+   public double diameter;
+   public double luaspermukaan;
+   public double volume;
+ 
+   
+   public void setjarijari(double jarijari){
+       this.jarijari = jarijari;
+   }
+   
+   
+   public void showDiameter(){
+       this.diameter = 2*jarijari;
+       
+   }
+   
+   public void showLuasPermukaan (){
+       this.luaspermukaan = 4*Math.PI*jarijari*jarijari;
+       
+   }   
+   
+   public void showVolume(){
+       this.volume = (4*Math.PI*jarijari*jarijari*jarijari)/3;
+       
+   }
+   
+   public void cetak(){
+       System.out.println("Jari-Jari : "+jarijari+" cm");
+       System.out.println("===========================");
+       System.out.println("Diameter  :"+diameter);
+       System.out.println("Luas Permukaan : "+luaspermukaan);
+       System.out.println("Volume : "+volume);
+       
+       
+       
+       
+   }
+}
